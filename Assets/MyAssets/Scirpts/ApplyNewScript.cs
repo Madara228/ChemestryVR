4c718c44c81e1f067944b9539f6d95af704f2f78
Madara228 <karagishiev14@mail.ru>
2018-12-05 19:37:10 +0300

    realese working creation

--- Assets/MyAssets/Scirpts/ApplyNewScript.cs
+++ Assets/MyAssets/Scirpts/ApplyNewScript.cs
@@ -22,8 +22,10 @@ public class ApplyNewScript : MonoBehaviour
 		for (int i = 0; i < _metaDataScript.data.Count; i++)
 		{
 			Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2),Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
-			var txt = ball.GetComponentInParent<TextMesh>();
-			Debug.Log(txt + "TextMesh");
+			GameObject ball_gobj = ball;
+			var txt = ball_gobj.GetComponentInChildren<TextMesh>();
+			txt.text = _metaDataScript.data[i].ToString();
+			Debug.Log(txt.text + "TextMesh");
 			Instantiate(ball,pos,Quaternion.identity);
 		}
 	}
