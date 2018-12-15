c045ea04cb938cd368525c5203ec74fa0f6e259e
Madara228 <karagishiev14@mail.ru>
2018-12-15 14:04:40 +0300

    fix bugs

--- Assets/MyAssets/Scirpts/ApplyNewScript.cs
+++ Assets/MyAssets/Scirpts/ApplyNewScript.cs
@@ -16,6 +16,7 @@ public class ApplyNewScript : MonoBehaviour
     private Renderer renderer;
     public Vector3 center, size;
     public GameObject line;
+    private Vector3 pos;
 
     void Start()
     {
@@ -43,33 +44,51 @@ public class ApplyNewScript : MonoBehaviour
 
     public void Apply()
     {
-        for (int i = 0; i < _metaDataScript.data.Count; i++)
-        {
-            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2),
-                              Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
-            //Рандомный инстанс
-            var txt = ball.GetComponentInChildren<TextMesh>();
-            txt.text = _metaDataScript.data[i].ToString();
-            //Получаем наш текст
-
-            if (i>0 && _metaDataScript.data[i] != _metaDataScript.data[i - 1])
-            {
-                //<Меняем цвет шариков>
-                var ball_gobj = Instantiate(ball, pos, Quaternion.identity);
-                var ball_renderer = ball_gobj.GetComponent<Renderer>();
-                ball_renderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
-                Debug.Log(ball_renderer.material.color);
-                Debug.Log("material render");
-                var gameobject = GameObject.FindGameObjectsWithTag("ball"); 
-                //</Меняем цвет шариков>
-            }
-            else
-            {
-                Instantiate(ball, pos, Quaternion.identity);
-                Debug.Log("not edited");
-            }
-
-        }
+        GameObject check_ball = GameObject.FindGameObjectWithTag("ball");
+                Debug.Log(check_ball);
+                if (check_ball == null)
+                {
+                    Debug.Log("CHECK - TRUE");
+                    for (int i = 0; i < _metaDataScript.data.Count; i++)
+                    {
+                        pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2),
+                                  Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+                        //Рандомный инстанс
+                        var txt = ball.GetComponentInChildren<TextMesh>();
+                        txt.text = _metaDataScript.data[i].ToString();
+                        //Получаем наш текст
+        
+                        if (i>0 && _metaDataScript.data[i] != _metaDataScript.data[i - 1])
+                        {
+                            //<Меняем цвет шариков>
+                            var ball_gobj = Instantiate(ball, pos, Quaternion.identity);
+                            var ball_renderer = ball_gobj.GetComponent<Renderer>();
+                            ball_renderer.material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+                            Debug.Log(ball_renderer.material.color);
+                            Debug.Log("material render");
+                            var gameobject = GameObject.FindGameObjectsWithTag("ball"); 
+                            //</Меняем цвет шариков>
+                        }
+                        else
+                        {
+                            GameObject instance =  Instantiate(ball, pos, Quaternion.identity);
+                            GameObject[] mass = GameObject.FindGameObjectsWithTag("ball");
+                            //check(mass,i);
+                            Debug.Log("not edited");
+                        }
+        
+                    }
+                }
+                else
+                {
+                    GameObject[] gobjery = GameObject.FindGameObjectsWithTag("ball");
+                    for (int i = 0; i < gobjery.Count(); i++)
+                    {
+                        
+                        Destroy(gobjery[i]);
+                    }
+                    Debug.Log("check - false");
+                }
     }
 
     private void OnDrawGizmos()
